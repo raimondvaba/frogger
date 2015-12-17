379787e56911ef74b7f1451a9a930ddf27293fcd
Taavi Tali <taavi.tali89@gmail.com>
2015-12-17 22:50:07 +0200

    Add GoalManagerTest

---
+++ test/frogger/GoalManagerTest.java
@@ -0,0 +1,49 @@
+package frogger;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import frogger.goal.GoalManager;
+
+public class GoalManagerTest {
+    
+    private GoalManager manager;
+
+    @Before
+    public void setUp() throws Exception {
+        manager = new GoalManager();
+    }
+
+    @Test
+    public void testGoalManager() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testInitializeGoalsPerLevel() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGet() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testAreAllGoalsReached() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testDoBonusCheck() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testUpdate() {
+        fail("Not yet implemented");
+    }
+
+}
