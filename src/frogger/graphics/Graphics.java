3be3aa51baa80601b96c8afd9619c22d04127ded
Taavi Tali <taavi.tali89@gmail.com>
2015-12-30 23:27:49 +0200

    add helper static functions used for getting sprite path

--- src/frogger/graphics/Graphics.java
+++ src/frogger/graphics/Graphics.java
@@ -1,5 +1,7 @@
 package frogger.graphics;
 
+import java.util.List;
+
 import frogger.World;
 import jig.engine.ImageResource;
 import jig.engine.PaintableCanvas;
@@ -10,7 +12,8 @@ import jig.engine.hli.ImageBackgroundLayer;
 public class Graphics {
     
     public static final String RSC_PATH = "resources/";
-    public static final String SPRITE_SHEET = RSC_PATH + "frogger_sprites.png";
+    public static final String SPRITE_SHEET = "frogger_sprites.png";
+    public static final String SPRITE_SHEET_PATH = RSC_PATH + SPRITE_SHEET;
     
     private ImageBackgroundLayer backgroundLayer;
 
@@ -21,11 +24,21 @@ public class Graphics {
     private void initialize() {
         ResourceFactory.getFactory().loadResources(RSC_PATH, "resources.xml");
 
-        ImageResource bkg = ResourceFactory.getFactory().getFrames(SPRITE_SHEET + "#background").get(0);
+        ImageResource bkg = ResourceFactory.getFactory().getFrames(getSpritePath("background")).get(0);
         setBackgroundLayer(new ImageBackgroundLayer(bkg, World.WORLD_WIDTH, World.WORLD_HEIGHT, ImageBackgroundLayer.TILE_IMAGE));
 
         PaintableCanvas.loadDefaultFrames("col", 30, 30, 2, JIGSHAPE.RECTANGLE, null);
         PaintableCanvas.loadDefaultFrames("colSmall", 4, 4, 2, JIGSHAPE.RECTANGLE, null);
+    }
+    
+    public static String getSpritePath(String resource) {
+        return SPRITE_SHEET_PATH + "#" + resource;
+    }
+    
+    public static List<ImageResource> getImageResources(String resourceName) {
+        String spritePath = getSpritePath(resourceName);
+        ResourceFactory factory = ResourceFactory.getFactory(); 
+        return factory.getFrames(spritePath);
     }
 
     public ImageBackgroundLayer getBackgroundLayer() {
